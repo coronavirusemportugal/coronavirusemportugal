0f3a0eb86d7c3f3173a3a4e3ee5e10930f26efe9
Bruno Tavares <eu@brunotavares.net>
2020-03-15 21:30:58 +0000

    New version

--- js/main.js
+++ js/main.js
@@ -1,59 +1,8 @@
-
-(function ($) {
-    "use strict";
-
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
-
-    $('.validate-form').on('submit',function(){
-        var check = true;
-
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
-
-        return check;
-    });
-
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
+$( document ).ready(function() {
 
     var chart_options = {
       chart: {
-        height: 380,
+        height: 305,
         width: '100%',
         type: "area",
         animations: {
@@ -71,22 +20,20 @@
       ],
       xaxis: {
         type: "datetime"
-      }
+      },
+      colors: ['#2E93fA', '#e74a3b', '#546E7A', '#E91E63', '#FF9800']
     };
     
     var chart = new ApexCharts(document.querySelector("#chart"), chart_options);
     
     chart.render();
 
-
-    
-    function getData() {
-        $.ajax({
-            url: "https://europe-west2-coronavirusportugal.cloudfunctions.net/data",
-            type: "GET",
-        })
-        .done(function(data) {
-           //alert("success");
+    $.ajax({
+        url: "https://europe-west2-coronavirusportugal.cloudfunctions.net/data",
+        type: "GET",
+    })
+    .done(function(data) {
+        //alert("success");
         var pt = JSON.parse(data);
         var cases      = pt["cases"];
         var deaths     = pt["deaths"];
@@ -103,92 +50,27 @@
         }
 
         var infected_series = [];
+        var deaths_series    = [];
         $.each(pt['history'] , function(index, val) {
           infected_series.push({x:val['date'],y:val['infected']});
+          deaths_series.push({x:val['date'],y:val['dead']});
         });
 
         // Fill in chart
-        chart.updateSeries([{
+        chart.updateSeries([
+          {
             name: "Infetados",
             data: infected_series
-        }]);
-    
-        })
-        .fail(function() {
-            console.log("error");
-        });
-    }
-    
-        // timer to refresh data automatically
-    function initTimer() {
-        var refreshDataDelay = 10000;
+          },
+          {
+            name: "Óbitos",
+            data: deaths_series
+          }
 
-        setInterval(function(){
-                getData();
-        },refreshDataDelay);
-    }
+        ]);
 
-
-    function chunk (arr, len) {
-
-        var chunks = [],
-            i = 0,
-            n = arr.length;
-      
-        while (i < n) {
-          chunks.push(arr.slice(i, i += len));
-        }
-      
-        return chunks;
-      }
-
-    
-    HandlebarsIntl.registerWith(Handlebars);
-
-    function getNews() {
-        $.ajax({
-            url: "https://us-central1-coronavirusportugal.cloudfunctions.net/news",
-            type: "GET",
-        })
-        .done(function(data) {
-           //alert("success");
-           var template = Handlebars.templates.news;
-           var newsarray = data.sort((a, b) => b.published - a.published)
-           $('#news').html(template(chunk(newsarray.slice(0, 10),2)));
-    
-        })
-        .fail(function() {
-            console.log("error");
-        });
-    }
-    getData();
-    getNews();
-    initTimer();
-
-    
-    
-    
-   /*==================================================================
-    [ Simple slide100 ]*/
-
-    $('.simpleslide100').each(function(){
-        var delay = 7000;
-        var speed = 1000;
-        var itemSlide = $(this).find('.simpleslide100-item');
-        var nowSlide = 0;
-
-        $(itemSlide).hide();
-        $(itemSlide[nowSlide]).show();
-        nowSlide++;
-        if(nowSlide >= itemSlide.length) {nowSlide = 0;}
-
-        setInterval(function(){
-            $(itemSlide).fadeOut(speed);
-            $(itemSlide[nowSlide]).fadeIn(speed);
-            nowSlide++;
-            if(nowSlide >= itemSlide.length) {nowSlide = 0;}
-        },delay);
+    })
+    .fail(function() {
+        console.log("error");
     });
-
-
-})(jQuery);
+});
